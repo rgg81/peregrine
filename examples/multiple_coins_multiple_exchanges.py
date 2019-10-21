70e60b956541b5d8ff278caeff1c9fe731b6101a
rgg81 <roberto.gomes@gmail.com>
2019-10-21 10:45:46 -0300

    New features

--- examples/multiple_coins_multiple_exchanges.py
+++ examples/multiple_coins_multiple_exchanges.py
@@ -2,7 +2,7 @@ from peregrinearb import create_weighted_multi_exchange_digraph, bellman_ford_mu
     print_profit_opportunity_for_path_multi
 
 
-graph = create_weighted_multi_exchange_digraph(['bittrex', 'gemini', 'kraken'], log=True)
+graph = create_weighted_multi_exchange_digraph(['binance'], log=True, fees=True)
 
 graph, paths = bellman_ford_multi(graph, 'ETH', loop_from_source=False, unique_paths=True)
 for path in paths:
---
+++ fcoin/WebsocketClient.py
@@ -0,0 +1,51 @@
+import json
+from websocket import create_connection
+from threading import Thread
+import time
+from datetime import datetime, timedelta
+
+class WebsocketClient():
+
+    last_update_date = datetime.now()
+
+    def __init__(self, ws_url="wss://api.fcoin.com/v2/ws",subprotocols=["binary","base64"]):
+        self.stop = False
+        self.url = ws_url
+        self.ws = create_connection(self.url)
+
+    def sub(self,topics):
+        self.open()
+        subParams = json.dumps(topics)
+        self.ws.send(subParams)
+        self.listen()
+
+    def open(self):
+        print("-- Subscribed! --")
+
+    def listen(self):
+        while not self.stop:
+            if self.last_update_date + timedelta(seconds=30) < datetime.now():
+                now_ms = int(time.time())
+                ping_message = {"cmd": "ping", "args": [now_ms], "id": "rgg81"}
+                self.ws.send(json.dumps(ping_message))
+                print('Sent a PING!!')
+                self.last_update_date = datetime.now()
+            try:
+                msg = json.loads(self.ws.recv())
+            except Exception as e:
+                print(e)
+                continue
+            else:
+                self.handle(msg)
+
+
+    def handle(self, msg):
+        #inherit method
+        print(msg)
+
+    def close(self):
+        self.ws.close()
+        self.closed()
+
+    def closed(self):
+        print("Socket Closed")
